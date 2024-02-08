e019efe0e3bc4cf46cf49d7156b8d4cd349181f8
Overseek <imanblmkdm@gmail.com>
2024-02-08 14:32:58 +0100

    Styling

--- src/quiz_app/card.tsx
+++ src/quiz_app/card.tsx
@@ -4,7 +4,17 @@ export default function Card() {
     <>
       <div className="container">
         <div className="quiz-app">
-
+            <h1>Simple Quiz</h1>
+            <div className="quiz">
+                <h2>my question</h2>
+                <div>
+                    <button className="btn">answer 1</button>
+                    <button className="btn">answer 2</button>
+                    <button className="btn">answer 3</button>
+                    <button className="btn">answer 4</button>
+                </div>
+                <button className="next-btn">Next</button>
+            </div>  
         </div>
       </div>
     </>
